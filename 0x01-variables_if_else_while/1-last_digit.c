41d7a546695a8acc5fc68f5939b8c6cfe0d1817a
OliverMaketso <122807237+OliverMaketso@users.noreply.github.com>
2023-09-08 19:34:16 +0300

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,33 +2,42 @@
 #include <stdlib.h>
 #include <time.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program generates a random number, extracts its last digit,
+ *              and prints whether it's greater than 5, equal to 0, or less than 6
+ *              but not 0.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    int n;
-    int lastDigit;
+int n;
+int lastDigit;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
-    /* Extract the last digit using modulo and ensure it's positive */
-    lastDigit = abs(n % 10);
+/* Extract the last digit and ensure its sign matches the original number */
+lastDigit = n % 10;
+if (lastDigit < 0)
+lastDigit *= -1;
 
-    printf("Last digit of %d is %d ", n, lastDigit);
+printf("Last digit of %d is %d ", n, lastDigit);
 
-    if (lastDigit > 5)
-    {
-        printf("and is greater than 5\n");
-    }
-    else if (lastDigit == 0)
-    {
-        printf("and is 0\n");
-    }
-    else
-    {
-        printf("and is less than 6 and not 0\n");
-    }
-
-    return (0);
+if (lastDigit > 5)
+{
+printf("and is greater than 5\n");
+}
+else if (lastDigit == 0)
+{
+printf("and is 0\n");
+}
+else
+{
+printf("and is less than 6 and not 0\n");
 }
 
-
+return (0);
+}
