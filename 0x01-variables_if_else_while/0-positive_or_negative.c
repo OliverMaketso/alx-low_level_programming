7fe34cdd67248dc7882256a3e2f87d654433b75d
Oliver Maketso <olivermaketso@gmail.com>
2023-09-08 06:14:46 -0700

    positive number

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("The number %d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("The number %d is zero\n", n);
+	}
+	else
+	{
+		printf("The number %d is negative\n", n);
+	}
+
+	return (0);
+}
+
