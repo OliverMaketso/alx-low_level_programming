aa85d29ddb1c66e11908fed4ac095fdbd0e74d19
OliverMaketso <122807237+OliverMaketso@users.noreply.github.com>
2023-09-13 23:53:44 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,19 +5,19 @@
  */
 void jack_bauer(void)
 {
-    int hour, minute;
+int hour, minute;
 
-    for (hour = 0; hour < 24; hour++)
-    {
-        for (minute = 0; minute < 60; minute++)
-        {
-            _putchar((hour / 10) + '0');    /* Print the tens digit of the hour */
-            _putchar((hour % 10) + '0');    /* Print the ones digit of the hour */
-            _putchar(':');
-            _putchar((minute / 10) + '0');  /* Print the tens digit of the minute */
-            _putchar((minute % 10) + '0');  /* Print the ones digit of the minute */
-            _putchar('\n');                 /* Print a newline character */
-        }
-    }
+for (hour = 0; hour < 24; hour++)
+{
+for (minute = 0; minute < 60; minute++)
+{
+_putchar((hour / 10) + '0');    /* Print the tens digit of the hour */
+_putchar((hour % 10) + '0');    /* Print the ones digit of the hour */
+_putchar(':');
+_putchar((minute / 10) + '0');  /* Print the tens digit of the minute */
+_putchar((minute % 10) + '0');  /* Print the ones digit of the minute */
+_putchar('\n');                 /* Print a newline character */
+}
+}
 }
 
