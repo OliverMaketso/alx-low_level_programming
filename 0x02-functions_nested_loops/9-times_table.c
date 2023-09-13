bff8e9eea20aa8f3919678187c93255e23684475
OliverMaketso <122807237+OliverMaketso@users.noreply.github.com>
2023-09-13 23:52:40 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,35 +5,35 @@
  */
 void times_table(void)
 {
-    int row, column, product, tens, ones;
+int row, column, product, tens, ones;
 
-    for (row = 0; row <= 9; row++)
-    {
-        for (column = 0; column <= 9; column++)
-        {
-            product = row * column;
+for (row = 0; row <= 9; row++)
+{
+for (column = 0; column <= 9; column++)
+{
+product = row * column;
 
-            if (column > 0)
-            {
-                _putchar(',');
-                _putchar(' ');
+if (column > 0)
+{
+_putchar(',');
+_putchar(' ');
             }
 
-            tens = product / 10;
-            ones = product % 10;
+tens = product / 10;
+ones = product % 10;
 
-            if (tens > 0)
-            {
-                _putchar(tens + '0');
-            }
-            else
-            {
-                _putchar(' ');
-            }
+if (tens > 0)
+{
+ _putchar(tens + '0');
+}
+else
+{
+_putchar(' ');
+}
 
-            _putchar(ones + '0');
-        }
-        _putchar('\n');
-    }
+_putchar(ones + '0');
+}
+_putchar('\n');
+}
 }
 
