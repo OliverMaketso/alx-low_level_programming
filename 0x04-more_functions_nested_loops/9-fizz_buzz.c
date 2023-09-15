445d4581f14979cc1692b042f352ab05d92d3dc2
OliverMaketso <122807237+OliverMaketso@users.noreply.github.com>
2023-09-15 14:11:58 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,22 +9,33 @@
  * Return: Always 0 (Success)
  */
 
-int main() {
+int main(void) {
 int i;
-for (i = 1; i <= 100; i++) {
-if (i % 3 == 0 && i % 5 == 0) {
+for (i = 1; i <= 100; i++) 
+{
+if (i % 3 == 0 && i % 5 == 0) 
+{
 printf("FizzBuzz");
-} else if (i % 3 == 0) {
+} 
+else if (i % 3 == 0) 
+{
 printf("Fizz");
-} else if (i % 5 == 0) {
+} 
+else if (i % 5 == 0) 
+{
 printf("Buzz");
-} else {
+} 
+else 
+{
 printf("%d", i);
 }
 
-if (i < 100) {
+if (i < 100) 
+{
 printf(" ");
-} else {
+} 
+else
+{
 printf("\n");
 }
 }
