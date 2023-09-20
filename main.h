cbb84570b77ccc132ad24d997b12d265c456b6bf
Oliver Maketso <olivermaketso@gmail.com>
2023-09-20 04:57:44 -0700

    m

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* This is an empty header file*/
+/* contains all the function prototypes of all the functions*/
+char *_strcat(char *dest, char *src);
+
+#endif /* MAIN_H*/
+
