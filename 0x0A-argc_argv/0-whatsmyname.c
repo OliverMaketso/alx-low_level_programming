438a0e31013674655515bdc1b9c19156e7890615
Oliver Maketso <olivermaketso@gmail.com>
2023-09-29 06:17:57 -0700

    m

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,44 +1,14 @@
-#include <unistd.h>
+#include <stdio.h>
+/*
+ * main - prints programme name
+ * @argc-argument count
+ * @argv- argument vector
+ * return 0
+*/
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
+int main(int __attribute__((unused)) argc, char *argv[])
 {
-	return (write(1, &c, 1));
+	printf("%s/n", argv[0]);
+	return (0);
 }
 
-#include "main.h"
-#include <stddef.h>
-
-/**
- * main - Entry point
- *
- * Description: Prints the name of the program, including the path.
- * If you rename the program, it will print the new name without recompilation.
- *
- * Return: Always 0 (success).
- */
-int main(int argc, char *argv[])
-{
-int i = 0;
-
-if (argc > 0 && argv[0] != NULL)
-{
-while (argv[0][i] != '\0')
-{
-_putchar(argv[0][i]);
-i++;
-}
-_putchar('\n');
-return (0);
-}
-else
-{
-return (1); /* Return an error code if argv[0] is not available */
-}
-}
